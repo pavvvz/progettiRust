d9e50fb1a4cbd393fc3016f80c4b3397a8cb0a8e
pavvvz <mattiap128129@gmail.com>
2025-04-15 11:43:15 +0200

    nuovo programma

---
+++ guessingGame/src/main.rs
@@ -0,0 +1,11 @@
+use std::io; //serve ad importare dalla standard library la libreria IO per l'input
+fn main() {
+    println!("Guess the number");
+    println!("Input the guess");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Something went wrong");
+
+    println!("The number you guessed is {}", guess)
+}
